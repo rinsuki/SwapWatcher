6a7c3589e32c73e50b7f8ba3a2a1c6458a4d45e5
rinsuki <428rinsuki+git@gmail.com>
2019-12-13 20:33:58 +0900

    Initial Commit

---
+++ SwapWatcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwapWatcher
+//
+//  Created by user on 2019/12/13.
+//  Copyright © 2019 rinsuki. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwapWatcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwapWatcher
+//
+//  Created by user on 2019/12/13.
+//  Copyright © 2019 rinsuki. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
